72cf7ac2d26b3a6c846772542fc12848cf4d5ab9
nehap12 <neha.paul@sjsu.edu>
2016-06-24 12:07:19 -0700

    second commit

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 var express = require('express'),
-    app = express();
+app = express();
+
+var path = require('path');
+app.use('/', express.static(__dirname + '/'));
+
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/db_mtaas');
 
 app.get('/', function(req, res) {
-  res.sendFile(__dirname+ '/views/login.html');
+  res.sendFile(__dirname+ '/public/login.html');
 });
 
 app.listen(3006, function(){
